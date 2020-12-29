d2ba52841c85f3b0db928c794ceb875c71d09a96
monsieurleberre <monsieurleberre@users.noreply.github.com>
2020-12-30 00:07:36 +0100

    latest dev changes to master (#14)
    * Try to remove warnings when possible
    
    * Adjust namespaces in workflow files
    
    * Remove warnings
    
    * Add : Tests for imported files
    
    * Fix : Remove duplicate function (#8)
    
    * Initial commit
    
    * Testing signature
    
    * Updated target framework and added classes
    
    * Update publish.nuget.yml
    
    * Update publish.nuget.yml
    
    * Fixed project path
    
    * Added missing imports to Trakx.Utils.Testing.csproj.
    
    * fix nuget publish
    
    Co-authored-by: AymericNoel <aym.noel@gmail.com>
    Co-authored-by: Carlos <carlos@takeapps.com>
    Co-authored-by: Carlos @ TakeApps <42281742+carlos-takeapps@users.noreply.github.com>

---
+++ src/Trakx.Utils.Testing/TestOutputHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Xunit.Abstractions;
+
+namespace Trakx.Utils.Testing
+{
+    public static class TestOutputHelperExtensions
+    {
+        public static string GetCurrentTestName(this ITestOutputHelper output)
+        {
+            var currentTest = output
+                .GetType()
+                .GetField("test", BindingFlags.Instance | BindingFlags.NonPublic)
+                ?.GetValue(output) as ITest;
+            if (currentTest == null)
+            {
+                throw new ArgumentNullException(
+                    $"Failed to reflect current test as {nameof(ITest)} from {nameof(output)}");
+            }
+
+            var currentTestName = currentTest.TestCase.TestMethod.Method.Name;
+            return currentTestName;
+        }
+    }
+}
