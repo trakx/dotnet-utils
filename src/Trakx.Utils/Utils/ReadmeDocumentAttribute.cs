38efcf762f89cda0590a7bc10eaf3c1c18594120
Carlos Salvatore <carlos@takeapps.com>
2020-12-30 13:25:20 -0300

    Added ReadmeDocumentAttribute for env stub file generation

---
+++ src/Trakx.Utils/Utils/ReadmeDocumentAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Trakx.Utils.Utils
+{
+    public class ReadmeDocumentAttribute : Attribute
+    {
+        public readonly string VarName;
+
+        public ReadmeDocumentAttribute(string varName)
+        {
+            VarName = varName;
+        }
+    }
+}
