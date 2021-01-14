ee23ddc5248daa7ff4b52199f8ab855f776d380b
carlos <carlos@takeapps.com>
2021-01-14 10:45:45 -0300

    Recovered latests changes. Missing tests, I will be writing them back today.

---
+++ src/Trakx.Utils.Testing.Tests/Unit/EnvFileDocumentationCreatorTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Trakx.Utils.Testing.Tests.Unit
+{
+    public class EnvFileDocumentationCreatorTests
+    {
+        public readonly ITestOutputHelper _output;
+
+        public EnvFileDocumentationCreatorTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        // no environment vars and no md should do nothing
+        [Fact]
+        public async Task NoEnvironmentVariablesWithoutMdFile()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+        // no environment vars and md with no previous entry should do nothing
+        [Fact]
+        public async Task NoEnvironmentVariablesWithMdFileNotIncludingShouldDoNothing()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+            creator.MdFileName = "ThisFileShouldNotExists.md";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+        // no environment vars and md with previous entry should delete lines
+        [Fact]
+        public async Task NoEnvironmentVariablesWithMdFileIncludingShouldDelete()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+            creator.MdFileName = "ThisFileShouldNotHaveEnvVars.md";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+        // environment vars and no md should create new file with lines
+        [Fact]
+        public async Task EnvironmentVariablesWithoutMdFileShouldCreate()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+            creator.MdFileName = "ThisFileShouldBeCreated.md";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+        // environment vars and md with no previous should insert lines
+        [Fact]
+        public async Task EnvironmentVariablesWitMdFileNotIncludingShouldInsert()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+            creator.MdFileName = "ThisFileShouldHaveNewLines.md";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+        // environment vars and md with previous should update lines
+        [Fact]
+        public async Task EnvironmentVariablesWitMdFileIncludingShouldUupdate()
+        {
+            var creator = new EnvFileDocumentationCreator(_output);
+            creator.DocumentedProjectName = "Trakx.Utils.Testing";
+            creator.MdFileName = "ThisFileShouldBeFixed.md";
+
+            var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
+        }
+
+    }
+}
--- src/Trakx.Utils.Testing/EnvFileDocumentationCreator.cs
+++ src/Trakx.Utils.Testing/EnvFileDocumentationCreator.cs
@@ -16,15 +16,20 @@ namespace Trakx.Utils.Testing
     {
         private readonly ITestOutputHelper _output;
 
+        public string? DocumentedProjectName { get; set; }
+        public string MdFileName { get; set; }
+
         public EnvFileDocumentationCreator(ITestOutputHelper output)
         {
             _output = output;
+            MdFileName = "Environment.md";
         }
 
         [Fact]
         public async Task TryCreateEnvFileDocumentation_should_create_env_template_file()
         {
             var creator = new EnvFileDocumentationCreator(_output);
+
             var envFileDocCreated = await creator.TryCreateEnvFileDocumentation().ConfigureAwait(false);
             envFileDocCreated.Should().BeTrue();
         }
@@ -34,27 +39,39 @@ namespace Trakx.Utils.Testing
             try
             {
                 var directory = GetAssemblyDirectory();
+
                 if (!directory.TryWalkBackToRepositoryRoot(out var repositoryRoot))
                     repositoryRoot.Should().NotBeNull("Tests should be running from its repository root.");
-                _output.WriteLine($"using {repositoryRoot!.FullName} as repository root path.");
-                var readmeFilePath = Path.Combine(repositoryRoot!.FullName, "Environment.md");
-                var configProperties = GetConfigurationProperties(directory);
 
-                if (!configProperties.Any()) return true;
+                _output.WriteLine($"using {repositoryRoot!.FullName} as repository root path.");
+
+                var readmeFilePath = Path.Combine(repositoryRoot!.FullName, MdFileName);
+                var environmentVariables = GetEnvironmentVariableNames();
 
                 var mdLines = new List<string>();
 
-                AddHeaderLines(mdLines);
+                DumpVarNamesByAssembly(mdLines, environmentVariables);
 
-                DumpVarNamesByAssembly(mdLines, configProperties);
-
-                AddFullSampleIntro(mdLines);
-
-                BuildSampleEnvFile(mdLines, configProperties);
-
-                if (!File.Exists(readmeFilePath)) File.Delete(readmeFilePath);
+                if (!File.Exists(readmeFilePath))
+                {
+                    if (environmentVariables.Any())
+                    {
+                        InsertHeaderLines(mdLines);
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    var currentFileLines = await ReadCurrentFileLinesAsync(readmeFilePath);
+                    ReplaceLinesInCurrentFile(currentFileLines, mdLines);
+                    mdLines = currentFileLines;
+                }
 
                 await File.WriteAllLinesAsync(readmeFilePath, mdLines);
+
                 return true;
             }
             catch (Exception e)
@@ -65,54 +82,55 @@ namespace Trakx.Utils.Testing
             }
         }
 
-        private void BuildSampleEnvFile(List<string> mdLines, Dictionary<string, List<string>> configProperties)
+        private async Task<List<string>> ReadCurrentFileLinesAsync(string readmeFilePath)
         {
-            configProperties
-                .Values
-                .SelectMany(x => x)
-                .Distinct()
-                .ToList()
-                .ForEach(x =>
+            var fileLines = await File.ReadAllLinesAsync(readmeFilePath);
+            return fileLines.ToList();
+        }
+
+        private void ReplaceLinesInCurrentFile(List<string> currentFileLines, List<string> mdLines)
+        {
+            var positionInCurrentFile = currentFileLines.IndexOf(mdLines[0]);
+            if (positionInCurrentFile > -1)
+            {
+                do
                 {
-                    mdLines.Add($"\t{x}");
+                    currentFileLines.RemoveAt(positionInCurrentFile);
+                }
+                while (currentFileLines.Count() > 0
+                && positionInCurrentFile < currentFileLines.Count() - 1
+                && !currentFileLines[positionInCurrentFile].StartsWith("### "));
+            }
+            else
+            {
+                positionInCurrentFile = currentFileLines.Count - 1;
+            }
+
+            currentFileLines.InsertRange(positionInCurrentFile, mdLines);
+        }
+
+        private void DumpVarNamesByAssembly(List<string> mdLines, List<string> environmentVariables)
+        {
+            if (string.IsNullOrEmpty(DocumentedProjectName))
+            {
+                DocumentedProjectName = Assembly.GetExecutingAssembly().GetName().Name;
+            }
+            mdLines.Add($"### {DocumentedProjectName}");
+
+            environmentVariables
+                .ForEach(propertyName =>
+                {
+                    mdLines.Add($"\t{propertyName}=********");
                 });
+
             mdLines.Add(string.Empty);
         }
 
-        private void DumpVarNamesByAssembly(List<string> mdLines, Dictionary<string, List<string>> configProperties)
+        private void InsertHeaderLines(IList<string> lines)
         {
-            configProperties
-                .Keys
-                .ToList()
-                .ForEach(key =>
-                {
-                    mdLines.Add($"### {key}");
-                    mdLines.Add(string.Empty);
-
-                    configProperties[key]
-                    .ToList()
-                    .ForEach(propertyName =>
-                    {
-                        mdLines.Add($"\t{propertyName}=********");
-                    });
-
-                });
-
-            mdLines.Add(string.Empty);
-        }
-
-        private void AddHeaderLines(IList<string> lines)
-        {
-            lines.Add("## Avoid committing you secrets and keys ");
-            lines.Add("In order to be able to run some integration tests, you should create a `.env` file in the `src` folder with the following items based on the Configuration classes you instantiate: ");
-            lines.Add(string.Empty);
-        }
-
-        private void AddFullSampleIntro(IList<string> lines)
-        {
-            lines.Add("### Complete .env file sample");
-            lines.Add(string.Empty);
-
+            lines.Insert(0, "## Avoid committing you secrets and keys ");
+            lines.Insert(1, "In order to be able to run some integration tests, you should create a `.env` file in the `src` folder with the following items: ");
+            lines.Insert(2, string.Empty);
         }
 
         private DirectoryInfo GetAssemblyDirectory()
@@ -121,9 +139,9 @@ namespace Trakx.Utils.Testing
             return new DirectoryInfo(codeBase)!.Parent!;
         }
 
-        private Dictionary<string, List<string>> GetConfigurationProperties(DirectoryInfo dirInfo)
+        private List<string> GetEnvironmentVariableNames()
         {
-            var result = new Dictionary<string, List<string>>();
+            var result = new HashSet<string>();
 
             var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.GetName().FullName.StartsWith("Trakx"));
 
@@ -137,28 +155,23 @@ namespace Trakx.Utils.Testing
                     {
                         var typename = x.FullName;
 
-                        if (string.IsNullOrEmpty(typename) || !typename.EndsWith("Configuration")) return;
-                        var varNames = new List<string>();
+                        if (string.IsNullOrEmpty(typename)) return;
 
                         var typeProperties = x.GetProperties();
 
                         if (typeProperties.Length <= 0) return;
+
                         foreach (var property in typeProperties)
                         {
                             if (property.GetCustomAttribute(typeof(ReadmeDocumentAttribute)) is ReadmeDocumentAttribute attribute)
                             {
-                                varNames.Add(attribute.VarName);
+                                result.Add(attribute.VarName);
                             }
-                        }
-
-                        if (varNames.Count > 0)
-                        {
-                            result.Add(typename, varNames);
                         }
                     });
                 });
 
-            return result;
+            return result.ToList();
         }
     }
 }
