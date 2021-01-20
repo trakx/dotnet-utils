959677b298eb25b220761378e0ed90810f212910
monsieurleberre <monsieur.leberre@gmail.com>
2021-01-20 08:06:18 +0000

    Change Secret provider to be a base class, add support for test ordering

---
+++ src/Trakx.Utils.Testing/Attributes/RunOrderAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Trakx.Utils.Testing.Attributes
+{
+    //https://docs.microsoft.com/en-us/dotnet/core/testing/order-unit-tests?pivots=xunit
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class RunOrderAttribute : Attribute
+    {
+        /// <summary>
+        /// The lower the value of <see cref="Order"/>, the earlier the test
+        /// will be placed in the queue.
+        /// </summary>
+        public double Order { get; }
+
+        public RunOrderAttribute(double priority) => Order = priority;
+    }
+}
