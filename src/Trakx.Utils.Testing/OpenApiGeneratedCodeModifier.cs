8544392a3b8983d8c9717ee2b693b11dbbe9736b
monsieurleberre <monsieur.leberre@gmail.com>
2021-02-16 12:03:18 +0000

    again

--- src/Trakx.Utils.Testing/OpenApiGeneratedCodeModifier.cs
+++ src/Trakx.Utils.Testing/OpenApiGeneratedCodeModifier.cs
@@ -15,7 +15,7 @@ namespace Trakx.Utils.Testing
     public abstract class OpenApiGeneratedCodeModifier
     {
         private readonly ITestOutputHelper _output;
-        private readonly string? _filePath;
+        private readonly string _filePath;
 
         private static readonly string ClassDefinitionRegex = @"(?<before>[\s]{4}\[System\.CodeDom\.Compiler\.GeneratedCode\(""NJsonSchema"", [^\]]+\]\r?\n[\s]{4}public partial )(?<class>class)(?<after> [\w]+\s?\r?\n)";
         private static readonly string SetPropertyRegex = @"(?<before>[\s]{8}\[Newtonsoft\.Json\.JsonProperty\([^\]]+\]\r?\n[\s]{8}public [^\s]+ [^\s]+ \{ get; )(?<set>set)";
@@ -30,16 +30,16 @@ namespace Trakx.Utils.Testing
         [Fact, RunOrder(1)]
         public async Task Replace_model_class_by_records()
         {
-            var content = await File.ReadAllTextAsync(_filePath!).ConfigureAwait(false);
+            var content = await File.ReadAllTextAsync(_filePath).ConfigureAwait(false);
             content = Regex.Replace(content, ClassDefinitionRegex, "${before}" + "record" + "${after}");
             content = Regex.Replace(content, SetPropertyRegex, "${before}" + "init");
-            await File.WriteAllTextAsync(_filePath!, content);
+            await File.WriteAllTextAsync(_filePath, content);
         }
 
         [Fact, RunOrder(2)]
-        public async Task Remove_warnings(string? filePath = default)
+        public async Task Remove_warnings()
         {
-            var content = await File.ReadAllTextAsync(filePath!).ConfigureAwait(false);
+            var content = await File.ReadAllTextAsync(_filePath).ConfigureAwait(false);
             var existingWarningStart = @"#pragma warning disable 108";
             var warningStartToAdd = @"#pragma warning disable CS0618";
             if (!content.Contains(warningStartToAdd)) content = content.Replace(existingWarningStart,
@@ -48,14 +48,14 @@ namespace Trakx.Utils.Testing
             var warningStopToAdd = @"#pragma warning restore CS0618";
             if (!content.Contains(warningStopToAdd)) content = content.Replace(existingWarningStop,
                 existingWarningStop + Environment.NewLine + warningStopToAdd);
-            await File.WriteAllTextAsync(filePath!, content);
+            await File.WriteAllTextAsync(_filePath, content);
         }
 
 
         [Fact, RunOrder(3)]
-        public async Task Output_client_names(string? filePath = default)
+        public async Task Output_client_names()
         {
-            var content = await File.ReadAllTextAsync(filePath!).ConfigureAwait(false);
+            var content = await File.ReadAllTextAsync(_filePath).ConfigureAwait(false);
             var clientRegex = new Regex(ClientRegex);
             var clients = clientRegex.Matches(content).Select(m => m.Groups["client"].Value)
                 .OrderBy(s => s);
