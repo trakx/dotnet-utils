1ad86a16da5ebf981ae5f3652f717ccdb90ae8a4
Rafael Melo <79855726+RafaelMeloTrakx@users.noreply.github.com>
2021-06-28 00:45:55 -0300

    Added IHasCreatedLastModified interface. (#35)

---
+++ src/Trakx.Utils/Persistence/IHasCreatedLastModified.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Trakx.Utils.Persistence
+{
+    public interface IHasCreatedLastModified
+    {
+        DateTimeOffset Created { get; set; }
+        DateTimeOffset LastModified { get; set; }
+    }
+}
